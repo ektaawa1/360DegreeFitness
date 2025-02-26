46a09e051693cc74a24d72ff7bbbffbf0b18a4fa
Mousse789 <119829466+Mousse789@users.noreply.github.com>
2025-02-25 21:05:20 -0800

    database update for meal diary and meal logs

--- backend/models/userMealLogger.py
+++ backend/models/userMealLogger.py
@@ -1,5 +1,5 @@
 from typing import Literal, List
-from pydantic import BaseModel, condecimal
+from pydantic import BaseModel, condecimal, computed_field
 from datetime import date
 
 
@@ -21,10 +21,37 @@ class UserMealLogger(BaseModel):
     protein_per_serving: condecimal(ge=0)
     meal_log_date: date
 
+    # Computed total fields based on number of servings consumed
+    @computed_field
+    @property
+    def total_calories(self) -> float:
+        return float(self.calories_per_serving * self.quantity_consumed)  # Total calories for all servings
+
+    @computed_field
+    @property
+    def total_fat(self) -> float:
+        return float(self.fat_per_serving * self.quantity_consumed)  # Total fat for all servings
+
+    @computed_field
+    @property
+    def total_carbs(self) -> float:
+        return float(self.carbs_per_serving * self.quantity_consumed)  # Total carbs for all servings
+
+    @computed_field
+    @property
+    def total_protein(self) -> float:
+        return float(self.protein_per_serving * self.quantity_consumed)  # Total protein for all servings
+
 class UserMealDiary(BaseModel):
+    user_id: str
     date: date
     breakfast: List[UserMealLogger] = []  # A List of breakfast logs
     lunch: List[UserMealLogger] = []      # A List of lunch logs
     dinner: List[UserMealLogger] = []     # A List of dinner logs
     snacks: List[UserMealLogger] = []     # A List of snacks logs
-
+    daily_nutrition_summary: dict = {
+        "total_calories": 0, # daily_calories_summary
+        "total_fat": 0,
+        "total_carbs": 0,
+        "total_protein": 0
+    }
